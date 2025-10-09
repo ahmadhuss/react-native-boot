d6cee72a7521417f508c8ee2602f776c87fb8791
ahmadhuss <ahmadhussnain787@gmail.com>
2025-10-09 23:28:46 +0500

    refactor: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,10 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
-const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
-const reactNative = require('eslint-plugin-react-native');
 
 module.exports = defineConfig([
   expoConfig,
-  eslintPluginPrettierRecommended,
-
-  // Register the plugin and rules
   {
-    plugins: {
-      'react-native': reactNative
-    },
-    rules: {
-      'react-native/no-unused-styles': 'error'
-    }
+    ignores: ['dist/*'],
   },
-
-  // keep ignores last
-  {
-    ignores: ['dist/*']
-  }
 ]);
