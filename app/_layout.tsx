c5aa9b50bccae59868e88af7c66f30f8a1a269da
ahmadhuss <ahmadhussnain787@gmail.com>
2025-10-19 16:42:22 +0500

    explore stack navigation with modal presentation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,15 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Shopping list" }}></Stack.Screen>
+      <Stack.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          presentation: "modal",
+          animation: "slide_from_bottom"
+        }}
+      ></Stack.Screen>
+      <Stack.Screen name="idea" options={{ title: "Idea" }}></Stack.Screen>
     </Stack>
   );
 }
--- app/counter.tsx
+++ app/counter.tsx
@@ -1,8 +1,17 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
 
 export default function Counter() {
+  const router = useRouter();
+
   return (
     <View style={styles.container}>
+      <TouchableOpacity onPress={() => router.navigate("/idea")}>
+        <Text style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}>
+          Go to the idea screen
+        </Text>
+      </TouchableOpacity>
+
       <Text style={styles.text}>Counter</Text>
     </View>
   );
